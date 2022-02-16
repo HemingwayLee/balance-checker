b82c73513f1a256a9488ee63d8b4463ad7ad8a9c
LeeHemingway <coherence0815@gmail.com>
2022-02-16 23:58:46 +0900

    add btc

--- run.js
+++ run.js
@@ -7,3 +7,11 @@ request('https://api.xrpscan.com/api/v1/account/rQDKjHYX8tytLNJYY1rkVeqDqRFP1Mxa
   }
 );
 
+request('https://blockchain.info/multiaddr?active=1AYPH5z2nXee5FkKBKvpYRTB2iZJRutTn2',
+  function (error, response, body) {
+    const balance = JSON.parse(body)["addresses"][0]["final_balance"];
+    console.log(`BTC balance: ${parseFloat(balance)/100000000.0}`);
+    // console.log(body);
+  }
+);
+
